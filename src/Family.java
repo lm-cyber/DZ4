be4da39ba8bbf6bc67c07e9e1ce3de69a3bada6b
lm-cyber <tyujn24@mail.ru>
2022-04-18 16:24:04 +0300

    я ничего не понимаю ,но вроде работает

--- src/Family.java
+++ src/Family.java
@@ -1,69 +1,119 @@
 import java.util.*;
 
 public class Family {
-    public static List<Integer> findOneStart(MatrixOfIntersection matrix) {
-        List<Integer> oneStart = new ArrayList<>();
-        for (int i = 1; i < matrix.kol; i++) {
-            if (matrix.matrix[i][0] == 1) {
-                oneStart.add(i);
 
-            }
+
+    public static Integer alfa=0;
+    public static List<SortedSet<Integer>> psi = new ArrayList<>();
+    public static byte[] byteOr(byte[] first,byte[] second) {
+        byte[] result = new byte[first.length];
+        for (int i = 0;i<first.length;i++) {
+            result[i]= (byte) (first[i] | second[i]);
         }
-        return oneStart;
+        return result;
     }
-
-    public static List<Integer> findZeroStart(MatrixOfIntersection matrix) {
-        List<Integer> zeroStart = new ArrayList<>();
-        for (int i = 1; i < matrix.kol; i++) {
-            if (matrix.matrix[i][0] == 0) {
-                zeroStart.add(i);
-
-            }
-        }
-        return zeroStart;
-    }
-
-    public static boolean chek(byte[] mat) {
-        for (byte i : mat) {
-            if (i == 0) {
+    private static boolean chek(byte[] mat) {
+        for(int i =0 ;i <mat.length;i++) {
+            if(mat[i] ==0)
+            {
                 return false;
             }
         }
         return true;
     }
-    public static byte[] dez(byte[] mat,byte[] dezmat) {
-        for (int i = 0; i< mat.length; i++) {
-            if(dezmat[i]==1) {
-                mat[i] = 1;
+    public static void findSet(SortedSet<Integer> set,byte[] matrix,Integer last,byte[][] matrixOfMatrix) {
+        if(chek(matrix)) {
+            alfa++;
+            psi.add(set);
+        }
+
+        for (int j = last+1;j<matrix.length;j++) {
+            if(matrix[j] == 0) {
+                set.add(j);
+                findSet(set,byteOr(matrix,matrixOfMatrix[j]),j,matrixOfMatrix);
             }
         }
-        return mat;
     }
+    public static List<List<String>> find(MatrixOfIntersection matrix) {
+        for(int i =0;i<matrix.matrix.length;i++) {
+            SortedSet<Integer> startSet = new TreeSet<>();
+            startSet.add(i);
 
-   /* public static  findS(Integer last,List<List<Integer>> lists,MatrixOfIntersection matrix,byte[] mat,List<Integer> zeroStart) {
+            findSet(startSet,matrix.matrix[i],i, matrix.matrix);
 
-        for (Integer number:
-             ) {
-            
         }
+        List<List<String>> fam = new ArrayList<>();
+
+        for(int i = 0; i< psi.size() ; i++) {
+            List<String> tmp = new ArrayList<>();
+            for (Integer nameNumber : psi.get(i)) {
+                tmp.add(matrix.names[nameNumber]);
+            }
+            fam.add(tmp);
+        }
+
+        return fam;
     }
-    public static List<List<Integer>> find(MatrixOfIntersection matrix) {
-        List<List<Integer>> lists = new ArrayList<List<Integer>>();
-        List<Integer> oneStart = findOneStart(matrix);
-        List<Integer> zeroStart = findZeroStart(matrix);
-
-        for (int i = 0; i < oneStart.size(); i++) {
-            for (int j =0;j<oneStart.size();j++) {
-                List<Integer> tmp = new ArrayList<>();
-                tmp.add(oneStart.get(i));
-                tmp.add(zeroStart.get(j));
-                byte[] mat = dez(matrix.matrix[oneStart.get(i)].clone(),matrix.matrix[tmp.get(1)]);
-
-
-
-                lists.addAll(findS(, , matrix, mat, zeroStart));
+    public static int set(List<List<String>> fam, int index, int jndex) {
+        int count = 0;
+        int iLen = fam.get(index).size();
+        int jLen = fam.get(jndex).size();
+        for (int i = 0; i < iLen; i++) {
+            for (int j = 0; j < jLen; j++) {
+                if (fam.get(index).get(i).equals(fam.get(jndex).get(j))) {
+                    count++;
+                }
             }
         }
-        return lists;
-    }*/
+        return count;
+    }
+
+
+    public static int[][] genTable(List<List<String>> fam) {
+        int count = fam.size();
+        int[][] table = new int[count][count];
+        for (int i = 0; i < count; i++) {
+            for (int j = i; j < count; j++) {
+                if (j == i) {
+                    table[i][j] = 0;
+                } else {
+                    table[i][j] = fam.get(i).size() + fam.get(j).size() - set(fam, i, j);
+                }
+
+
+            }
+        }
+
+
+        return table;
+    }
+
+    public static int[] getMax(int[][] table) {
+        int count = table.length;
+        int max = 0;
+        int[] maxVer = new int[2];
+        for (int i = 0; i < count; i++) {
+            for (int j = i; j < count; j++) {
+                if(table[i][j]>max) {
+                    max= table[i][j];
+                    maxVer[0] = i;
+                    maxVer[1] = j;
+                }
+
+            }
+        }
+
+        return maxVer;
+
+    }
+    public static List<List<String>> delete(List<List<String>> fam ,int[] maxVer) {
+        List<String> copy1 =new ArrayList<>(fam.get(maxVer[0]));
+        List<String> copy2 =new ArrayList<>(fam.get(maxVer[1]));
+        for (List<String> psiI : fam) {
+            psiI.removeAll(copy1);
+            psiI.removeAll(copy2);
+        }
+
+        return fam;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -5,21 +6,21 @@ public class Main {
         Matrix matrix = new Matrix();
 
 
-        List<Integer> test = matrix.findGamiltonCikl();
-        for (Integer integer :  test) {
-            Integer p = integer+1 ;
-            System.out.print(prob(p.toString().length(),3)+p );
+        List<Integer> gamiltonCikl = matrix.findGamiltonCikl();
+        for (Integer integer : gamiltonCikl) {
+            Integer p = integer + 1;
+            System.out.print(prob(p.toString().length(), 3) + p);
         }
         System.out.println();
-        for(Integer i = 0 ; i< 12 ;i++){
-            Integer p = i+1 ;
-            System.out.print( prob(p.toString().length(),3)+p);
+        for (Integer i = 0; i < 12; i++) {
+            Integer p = i + 1;
+            System.out.print(prob(p.toString().length(), 3) + p);
         }
         System.out.println();
         System.out.println();
-         for(int i = 0;i<12;i++) {
-            for (int j =0 ;j<12;j++) {
-                System.out.print(Matrix.matrix[i][j]+" ");
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                System.out.print(Matrix.matrix[i][j] + " ");
 
             }
             System.out.println();
@@ -27,52 +28,161 @@ public class Main {
 
         System.out.println();
         Matrix.changeMatrix();
-         for(int i = 0;i<12;i++) {
-            for (int j =0 ;j<12;j++) {
-                System.out.print(Matrix.newMatrix[i][j]+" ");
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                System.out.print(Matrix.newMatrix[i][j] + " ");
 
             }
             System.out.println();
         }
+//        Matrix.newMatrix = new byte[][]{
+//                {0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 0},
+//                {0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1},
+//                {0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+//                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+//        };
+
         MatrixOfIntersection matrixOfIntersection = matrix.matrixOfIntersection();
         String[] strings = matrixOfIntersection.names;
         int max = matrix.kol();
         System.out.print("      ");
-        for (int i =0;i<max;i++)
-        {
+        for (int i = 0; i < max; i++) {
 
-            System.out.print(prob(strings[i].length())+strings[i] );
+            System.out.print(prob(strings[i].length()) + strings[i]);
 
         }
         System.out.println();
-        for (int i=0;i<max; i++) {
-            System.out.print(prob(strings[i].length())+strings[i] );
-            for (int j=0;j<max;j++)
-            {
+        for (int i = 0; i < max; i++) {
+            System.out.print(prob(strings[i].length()) + strings[i]);
+            for (int j = 0; j < max; j++) {
                 System.out.print("     " + matrixOfIntersection.matrix[i][j]);
             }
             System.out.println();
         }
+
+        List<List<String>> psi = Family.find(matrixOfIntersection);
+        int indexPsi = 1;
+        for (List<String> psiIndex: psi) {
+            System.out.print("Psi" + indexPsi +" :");
+            indexPsi++;
+            for (String rebro : psiIndex) {
+                System.out.print(rebro+" ");
+            }
+            System.out.println();
+        }
+        int[][] table = Family.genTable(psi);
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                System.out.print(prob(Integer.toString(table[i][j]).length(),3)+table[i][j]);
+            }
+            System.out.println();
+        }
+        int[] maxVer = Family.getMax(table);
+        System.out.println("max: "+(maxVer[0] + 1) + " " + (maxVer[1] + 1));
+        System.out.print("psi"+(maxVer[0]+1)+ " ");
+         for (String rebro : psi.get(maxVer[0])) {
+                System.out.print(rebro+" ");
+         }
+        System.out.print("\npsi"+(maxVer[1]+1)+" ");
+         for (String rebro : psi.get(maxVer[1])) {
+                System.out.print(rebro+" ");
+         }
+         System.out.println();
+         psi= Family.delete(psi,maxVer);
+         indexPsi=1;
+          for (List<String> psiIndex: psi) {
+            System.out.print("Psi" + indexPsi +" :");
+            indexPsi++;
+            for (String rebro : psiIndex) {
+                System.out.print(rebro+" ");
+            }
+            System.out.println();
+        }
+        /*
+        List<List<String>> test = new ArrayList<>();
+        List<String> tmp = new ArrayList<>();
+        tmp.add("1-3");
+        tmp.add("3-7");
+        tmp.add("3-6");
+        tmp.add("3-5");
+        test.add(tmp);
+        tmp = new ArrayList<>();
+        tmp.add("1-3");
+        tmp.add("3-7");
+        tmp.add("3-5");
+        tmp.add("5-7");
+        test.add(tmp);
+        tmp = new ArrayList<>();
+        tmp.add("1-3");
+        tmp.add("3-7");
+        tmp.add("4-7");
+        tmp.add("5-7");
+        test.add(tmp);
+        tmp = new ArrayList<>();
+        tmp.add("2-6");
+        tmp.add("2-4");
+        test.add(tmp);
+        tmp = new ArrayList<>();
+        tmp.add("2-6");
+        tmp.add("3-6");
+        tmp.add("3-5");
+        test.add(tmp);
+        tmp = new ArrayList<>();
+        tmp.add("2-4");
+        tmp.add("4-7");
+        tmp.add("5-7");
+        test.add(tmp);
+        int[][] table = Family.genTable(test);
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table[i].length; j++) {
+                System.out.print(table[i][j] + " ");
+            }
+            System.out.println();
+        }
+        int[] maxVer = Family.getMax(table);
+        System.out.println(maxVer[0] + 1 + " " + (maxVer[1] + 1));
+        List<String> reb = new ArrayList<>();
+        reb.add("2-6");
+        reb.add("1-3");
+        reb.add("5-7");
+        reb.add("2-4");
+        reb.add("3-7");
+        reb.add("3-6");
+        reb.add("3-5");
+        reb.add("4-7");
+        List<String> rebToChange = Family.delete(test, reb, maxVer);
+        for (String rebro : rebToChange) {
+            System.out.println(rebro);
+
+        }
+
+       */
     }
-    public static String prob(int i)
-    {
-        String s="";
-        while (i!=6){
-            s+=" ";
+
+
+    public static String prob(int i) {
+        String s = "";
+        while (i != 6) {
+            s += " ";
             i++;
         }
         return s;
     }
-    public static String prob(int i,int probs)
-    {
-        String s="";
-        while (i!=probs){
-            s+=" ";
+
+    public static String prob(int i, int probs) {
+        String s = "";
+        while (i != probs) {
+            s += " ";
             i++;
         }
         return s;
-    }
-    public static void test(Integer i ) {
-        i++;
     }
 }
